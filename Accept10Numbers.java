a1439f05f03fef4d8c131984bfec2f96ffa0d8fa
Collins Michael <144531933+Aeyjjjk@users.noreply.github.com>
2026-05-12 06:22:04 +0100

    Fix formatting in Accept10Numbers.java

--- Accept10Numbers.java
+++ Accept10Numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Accept10Numbers { public static void main(String[] args) { 
+public class Accept10Numbers { public static void main(String[] args) {  
 		Scanner input = new Scanner(System.in);
 
 		int sum6to9 = 0;
