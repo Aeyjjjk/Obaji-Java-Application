06a581376fa6b0cfaaaf13e9698ada4ac612232f
Collins Michael <144531933+Aeyjjjk@users.noreply.github.com>
2026-05-16 11:53:06 +0100

    update

--- Accept10Numbers.java
+++ Accept10Numbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Accept10Numbers { public static void main(String[] args) {  
 		Scanner input = new Scanner(System.in);
-
+ 
 		int sum6to9 = 0;
 		int sum4to10 = 0;
 		int differnce;
